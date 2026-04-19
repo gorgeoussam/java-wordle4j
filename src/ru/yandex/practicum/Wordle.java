5315417100f1eea2eebe21518eac4731639bfa57
Syoma Lukashenko <samlukashenko@gmail.com>
2026-04-19 19:15:58 +0300

    small fixes

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -6,15 +6,7 @@ import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 
-/*
-в главном классе нам нужно:
-    создать лог-файл (он должен передаваться во все классы)
-    создать загрузчик словарей WordleDictionaryLoader
-    загрузить словарь WordleDictionary с помощью класса WordleDictionaryLoader
-    затем создать игру WordleGame и передать ей словарь
-    вызвать игровой метод в котором в цикле опрашивать пользователя и передавать информацию в игру
-    вывести состояние игры и конечный результат
- */
+
 public class Wordle {
 
     private static final String DICTIONARY_FILE = "words_ru.txt";
